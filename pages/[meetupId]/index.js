43183ecca07761ae73e4275fb02bde0aa41cd36f
chistorillo <ian.historillo@gmail.com>
2021-05-31 01:24:37 +0800

    modifying fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -38,7 +38,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
